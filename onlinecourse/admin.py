d6caba41cadf4237029fe6dc60864a58dfa22db2
PeterSchuler1 <schulepr@mail.uc.edu>
2026-04-02 11:01:44 -0400

    Enhance admin interface with new models and inlines

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,8 +1,41 @@
 from django.contrib import admin
-from .models import Course, Enrollment, Question, Choice, Submission
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
-admin.site.register(Course)
-admin.site.register(Enrollment)
-admin.site.register(Question)
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
+
+
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    extra = 5
+
+
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [LessonInline, QuestionInline]
+    list_display = ('name', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['name', 'description']
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Instructor)
+admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
 admin.site.register(Submission)
